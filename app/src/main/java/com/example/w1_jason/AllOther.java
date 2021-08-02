2b534b71c9ca91aba039ca0a33adadf3b3091e37
sourog2000 <sourog2000@gmail.com>
2021-08-02 21:01:21 +0530

    HelloA

---
+++ app/src/main/java/com/example/w1_jason/AllOther.java
@@ -0,0 +1,18 @@
+package com.example.w1_jason;
+
+public class AllOther {
+    public String name;
+    public int id;
+    public String icon;
+
+    void setName(String na){
+        this.name = na;
+    }
+    void setId(int id){
+        this.id = id;
+    }
+    void setIcon(String ico){
+        this.icon = ico;
+    }
+
+}
---
+++ app/src/main/java/com/example/w1_jason/ExcluIt.java
@@ -0,0 +1,15 @@
+package com.example.w1_jason;
+
+public class ExcluIt {
+    public int fac_id;
+    public int opt_id;
+
+    void setFac_id(int i){
+        this.fac_id = i;
+    }
+
+    void setOpt_id(int j){
+        this.opt_id = j;
+    }
+
+}
